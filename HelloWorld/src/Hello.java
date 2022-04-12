eb9c6e30ecb3c954f262c42669292d35a914cf9c
keke1946 <asadrazanayani@gmail.com>
2022-04-11 23:18:28 -0400

    Initial Commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    // the bottom main is the entry point for any java code.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+    }
+
+}
